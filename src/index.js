7ed5b7269a09a1b1c90e4823786ef7ffa80bc276
只是蘋果JustApple <114324056+JustappleJust@users.noreply.github.com>
2025-02-09 10:37:39 +0800

    Ｕpdate image display method

--- src/index.js
+++ src/index.js
@@ -30,7 +30,6 @@ async function updateStatus() {
 	try {
 		//get status
 		let isOn = false;
-		let needUpdate = false;
 		let status = {};
 		const time = Date.now();
 		try {
@@ -42,32 +41,17 @@ async function updateStatus() {
 		
 		const ping = Date.now() - time;
 		
-		//update motd image
-		if ((lastId !== Math.round(time/600000)) && isOn) {
-			needUpdate = true;
-			motd = (await request.request('GET', `https://sr-api.sfirew.com/server/cowgl.xyz/banner/motd.png?hl=tw&v=${Math.round(time/600000)}&ping=false&mc_font=true`)).body;
-			lastId = Math.round(time/600000);
-		}
-		
 		//update status message
 		const res = (await client.apiRequestMultipart('PATCH', `/channels/${config.channelId}/messages/${config.messageId}`, {
 			embeds: [
 				{
 					title: 'CowGL.xyz 伺服器狀態',
 					description: isOn ? `✅ **上線中**\n上次檢查：<t:${Math.round(time/1000)}>\n玩家數：\`${status.players.online}\` / \`${status.players.max}\`\nPing：\`${ping}\` ms` : `🛑 **離線**\n上次檢查：<t:${Math.round(time/1000)}>`,
-					...(isOn ? { image: { url: 'attachment://motd.png' } } : {}),
+					...(isOn ? { image: { url: `https://sr-api.sfirew.com/server/cowgl.xyz/banner/motd.png?hl=tw&v=${Math.round(time/600000)}&ping=false&mc_font=true` } } : {}),
 					color: isOn ? 0x70e000 : 0xef233c
 				}
-			],
-			attachments: isOn ? [{ id: 0 }] : []
-		}, isOn ? [
-			{
-				id: 0,
-				type: 'image/png',
-				name: 'motd.png',
-				data: motd
-			}
-		] : [])).json();
+			]
+		})).json();
 	} catch (err) { console.error(err); }
 	
 	return;
